dd1990f4cb140105ab56aea9b6b14273867c261a
Tobias <tobiasorban00@gmail.com>
2026-04-20 07:07:24 -0300

    refactor: reorganizar estructura del proyecto en arquitectura por capas

---
+++ database/seed.sql
@@ -0,0 +1,5 @@
+-- seed.sql
+-- Datos de prueba iniciales (ej. roles de médico, enfermero y administrativo).
+
+-- Ejemplo:
+-- INSERT INTO roles (nombre) VALUES ('medico'), ('enfermero'), ('administrativo');
